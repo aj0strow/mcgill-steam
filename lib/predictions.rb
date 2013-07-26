d124aede80dc33543e25bf5bac2626d59c7e4539
aj0strow <alexander.ostrow@gmail.com>
2013-07-26 12:55:51 -0400

    generate csv working

--- lib/predictions.rb
+++ lib/predictions.rb
@@ -1,5 +1,44 @@
 module Predictions
-  def self.generate_training_data
-    PastRecord.all(:steam.not => nil).to_csv
+  class << self
+    
+    def generate_training_csv
+      CSV.generate do |csv|
+        csv << %w(Date Hour Temp Radiation Humidity WindSpeed Steam)
+        normalized_past_records.each do |record|
+          csv << record
+        end
+      end
+    end
+    
+    
+    private
+    
+    
+    def normalized_past_records
+      complete_past_records.map do |past_record|
+        normalize(past_record)
+      end
+    end
+    
+    def complete_past_records
+      PastRecord.all(:steam.not => nil)
+    end
+    
+    def normalize(past_record)
+      recorded_at = past_record.recorded_at
+      date_time = recorded_at.strftime('%m/%d/%Y ') + recorded_at.strftime('%k:%M').strip
+      hour = recorded_at.strftime('%k').strip.next
+      normalized = [ 
+        date_time, 
+        hour,
+        past_record.temperature,
+        past_record.radiation.floor,
+        past_record.humidity,
+        past_record.wind_speed,
+        past_record.steam.floor 
+      ]
+      normalized
+    end
+    
   end
 end
--- test/tasks/predictions_test.rb
+++ test/tasks/predictions_test.rb
@@ -2,9 +2,53 @@ require 'test_helper'
 
 class PredictionsTest < Test
   
+  setup do
+    @past_record_attributes = {
+      recorded_at: DateTime.parse('2012-10-19T01:00Z'), 
+      temperature: 13.8, 
+      radiation: -39, 
+      humidity: 0.61, 
+      wind_speed: 3.611111111, 
+      steam: 9300
+    }
+  end
+  
+  test 'normalize past_record for csv' do
+    @past_record = PastRecord.create(@past_record_attributes)
+    expected = [ '10/19/2012 1:00', '2', 13.8, -39, 0.61, 3.611111111, 9300 ]
+    normalized = Predictions.send(:normalize, @past_record)
+    assert_equal expected, normalized
+  end
+  
+  test 'complete past records are those with steam' do
+    PastRecord.all.destroy
+    PastRecord.create(@past_record_attributes)
+    @past_record_attributes[:recorded_at] = DateTime.parse('2012-10-20T01:00Z')
+    @past_record_attributes[:steam] = nil
+    PastRecord.create(@past_record_attributes)
+    complete_records = Predictions.send(:complete_past_records)
+    assert_equal complete_records.count, 1
+  end
+
   test 'generate CSV' do
-    training_data = Predictions.generate_training_data
-    assert_equal training_data.split("\n"), 'Date,Hour,Temp,Radiation,Humidity,WindSpeed,Steam'
+    PastRecord.all.destroy
+    records = [
+      [ DateTime.parse('2012-10-19T01:00Z'), 13.8, -39, 0.61, 3.611111111, 9300 ],
+      [ DateTime.parse('2012-10-19T02:00Z'), 12.5, -41, 0.59, 3.611111111, 9300 ],
+      [ DateTime.parse('2012-10-19T03:00Z'), 11.9, -42, 0.64, 3.611111111, 8400 ]
+    ]
+    keys = %w(recorded_at temperature radiation humidity wind_speed steam)
+    records.each do |record|
+      PastRecord.create(keys.zip(record))
+    end
+    expected = [
+      'Date,Hour,Temp,Radiation,Humidity,WindSpeed,Steam',
+      '10/19/2012 1:00,2,13.8,-39,0.61,3.611111111,9300',
+      '10/19/2012 2:00,3,12.5,-41,0.59,3.611111111,9300',
+      '10/19/2012 3:00,4,11.9,-42,0.64,3.611111111,8400', ''
+    ].join("\n")
+    csv = Predictions.send(:generate_training_csv)
+    assert_equal expected, csv
   end
   
   
